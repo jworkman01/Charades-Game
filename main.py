b58fd534760eccc3b9b89f71746fc6ee07d89939
mhilgen <max.hilgenberg@gmx.net>
2021-11-30 10:19:24 -0500

    Exit message

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+while True:
+    print("Test")
+    print("Would you like to play a new game? (Y / N)")
+    input1 = input()
+    if(input1 == 'N' or input1 == 'n'):
+        print("Thank you for playing!")
+        break
