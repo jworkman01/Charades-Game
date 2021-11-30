b5beb3fb6559c68990d3a92efeb2f01b01af78e9
mhilgen <max.hilgenberg@gmx.net>
2021-11-30 11:03:45 -0500

    added random implementation. Need the API to get the words now that we can put in line 7

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from tkinter import *
 import tkinter.messagebox
+import random
 root=Tk()
-i = 0
-while True:
 
-    question = tkinter.messagebox.showinfo('Your Word','This is your word!\n' + str(i) + "\nIf the word was guessed press OK")
-    end = tkinter.messagebox.askquestion('Are you done', 'Would you like to exit this game?')
-    if(end == 'yes'):
+
+while True:
+    random_value = random.randrange(1, 5)
+    question = tkinter.messagebox.showinfo('Your Word','This is your word!\n:' + str(random_value) + "\nIf done or repitition press OK")
+    end = tkinter.messagebox.askquestion('Are you done', 'Would you like to play another round?')
+    if(end == 'no'):
         question = tkinter.messagebox.showinfo('Good Bye','Thank you for playing!')
         break
-    elif(end == 'no'):
-        i = i + 1
-        
